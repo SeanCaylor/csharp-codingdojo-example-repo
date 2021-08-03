57e738932f2f9c743ba6e707e1a40a7b12569084
Neil M <neilm813@gmail.com>
2021-08-03 09:00:35 -0700

    algos

--- algorithms/data_structures/SinglyLinkedList.js
+++ algorithms/data_structures/SinglyLinkedList.js
@@ -112,6 +112,32 @@ class SinglyLinkedList {
     runner.next = newBack;
     return this;
   }
+
+  /**
+   * Creates a new node with the given data and inserts that node at the front
+   * of this list.
+   * - Time: (?).
+   * - Space: (?).
+   * @param {any} data The data for the new node.
+   * @returns {SinglyLinkedList} This list.
+   */
+  insertAtFront(data) {}
+
+  /**
+   * Removes the first node of this list.
+   * - Time: (?).
+   * - Space: (?).
+   * @returns {any} The data from the removed node.
+   */
+  removeHead() {}
+
+  /**
+   * Calculates the average of this list.
+   * - Time: (?).
+   * - Space: (?).
+   * @returns {number|NaN} The average of the node's data.
+   */
+  average() {}
 }
 
 const emptyList = new SinglyLinkedList();
