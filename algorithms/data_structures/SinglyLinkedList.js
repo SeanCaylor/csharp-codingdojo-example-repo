eb46b1c2b6250a11d466bf07b0ef17e07d9fb488
Neil M <neilm813@gmail.com>
2021-08-06 09:02:17 -0700

    algos

--- algorithms/data_structures/SinglyLinkedList.js
+++ algorithms/data_structures/SinglyLinkedList.js
@@ -389,6 +389,38 @@ class SinglyLinkedList {
       runner = runner.next;
     }
   }
+
+  /**
+   * Concatenates the nodes of a given list onto the back of this list.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {SinglyLinkedList} addList An instance of a different list whose
+   *    whose nodes will be added to the back of this list.
+   * @returns {SinglyLinkedList} This list with the added nodes.
+   */
+  concat(addList) {}
+
+  /**
+   * Finds the node with the smallest number as data and moves it to the front
+   * of this list.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @returns {SinglyLinkedList} This list.
+   */
+  moveMinToFront() {}
+
+  /**
+   * Splits this list into two lists where the 2nd list starts with the node
+   * that has the given value.
+   * splitOnVal(5) for the list (1=>3=>5=>2=>4) will change list to (1=>3)
+   * and the return value will be a new list containing (5=>2=>4)
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} val The value in the node that the list should be split on.
+   * @returns {SinglyLinkedList} The split list containing the nodes that are
+   *    no longer in this list.
+   */
+  splitOnVal(val) {}
 }
 
 const emptyList = new SinglyLinkedList();
