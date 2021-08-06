47cc2e2bb295a8a1cd561d3c510a7a9a7621b2f9
Neil M <neilm813@gmail.com>
2021-08-06 11:17:19 -0700

    viewmodel

--- ASPIntro/Controllers/HomeController.cs
+++ ASPIntro/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using ASPIntro.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPIntro.Controllers
@@ -23,10 +24,32 @@ namespace ASPIntro.Controllers
             };
 
             // This ViewBag only exists for this method and this rendered View.
-            ViewBag.YoutubeVideoIds = youtubeVideoIds;
-            ViewBag.RandomNumber = new System.Random().Next();
+            // ViewBag.YoutubeVideoIds = youtubeVideoIds;
+            // ViewBag.RandomNumber = new System.Random().Next();
 
-            return View("Videos");
+            ViewBag.Message = "You can use the ViewBag & a ViewModel together!";
+
+            VideosView viewModel = new VideosView()
+            {
+                YoutubeVideoIds = youtubeVideoIds,
+                RandomNumber = new System.Random().Next()
+            };
+
+            return View("Videos", viewModel);
+        }
+
+        // 1. Get the page with the registration <form>
+        [HttpGet("/users/register")]
+        public ViewResult Register()
+        {
+            return View("Register");
+        }
+
+        // 2. Handle the POST from the registration <form> submission
+        [HttpPost("/users/process-registration")]
+        public ViewResult ProcessRegistration(User newUser)
+        {
+            return View("Guest", newUser);
         }
 
         [HttpGet("{**path}")]
@@ -36,6 +59,23 @@ namespace ASPIntro.Controllers
             return RedirectToAction("Index");
         }
 
+        // EXTRA EXAMPLE: ViewModel used for both displaying and creating data.
+        [HttpGet("/users/register2")]
+        public ViewResult Register2()
+        {
+            RegisterView viewModel = new RegisterView()
+            {
+                NewUser = new User(),
+                Message = "Hello world from the view model."
+            };
 
+            return View("Register2", viewModel);
+        }
+
+        [HttpPost("/users/process-registration2")]
+        public ViewResult ProcessRegistration2(RegisterView viewModel)
+        {
+            return View("Guest", viewModel.NewUser);
+        }
     }
 }
