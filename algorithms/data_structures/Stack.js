820c57f60e49392e714c86d97c7325fe5bcd19c4
Neil M <neilm813@gmail.com>
2021-08-09 09:55:39 -0700

    algos

--- algorithms/data_structures/Stack.js
+++ algorithms/data_structures/Stack.js
@@ -10,8 +10,8 @@ class Stack {
    * @returns {Stack} This new Stack instance is returned without having to
    *    explicitly write 'return' (implicit return).
    */
-  constructor() {
-    this.items = [];
+  constructor(items = []) {
+    this.items = items;
   }
 
   /**
@@ -21,7 +21,10 @@ class Stack {
    * @param {any} item The new item to be added to the top / back.
    * @returns {number} The new length of this stack.
    */
-  push(item) {}
+  push(item) {
+    this.items.push(item);
+    return this.size();
+  }
 
   /**
    * Removes the top / last item from this stack.
@@ -29,7 +32,9 @@ class Stack {
    * - Space: O(1) constant.
    * @returns {any} The removed item or undefined if this stack was empty.
    */
-  pop() {}
+  pop() {
+    return this.items.pop();
+  }
 
   /**
    * Retrieves the top / last item from this stack without removing it.
@@ -37,7 +42,9 @@ class Stack {
    * - Space: O(1) constant.
    * @returns {any} The top / last item of this stack.
    */
-  peek() {}
+  peek() {
+    return this.items[this.items.length - 1];
+  }
 
   /**
    * Returns whether or not this stack is empty.
@@ -45,7 +52,9 @@ class Stack {
    * - Space: O(1) constant.
    * @returns {boolean}
    */
-  isEmpty() {}
+  isEmpty() {
+    return this.items.length === 0;
+  }
 
   /**
    * Returns the size of this stack.
@@ -53,7 +62,21 @@ class Stack {
    * - Space: O(1) constant.
    * @returns {number} The length.
    */
-  size() {}
+  size() {
+    return this.items.length;
+  }
+
+  /**
+   * Logs the items as a space separated string.
+   * - Time: O(n) linear.
+   * - Space: O(n) linear.
+   * @returns {string} The same string that is logged.
+   */
+  print() {
+    const str = this.items.join(" ");
+    console.log(str);
+    return str;
+  }
 }
 
 class Node {
@@ -66,5 +89,77 @@ class Node {
 class LinkedListStack {
   constructor() {
     this.head = null;
+  }
+
+  /**
+   * Adds a new item to the top of the stack (the head).
+   * - Time: O(1) constant.
+   * - Space: O(1).
+   * @param {any} val The val to add.
+   * @returns {void}
+   */
+  push(val) {
+    const newNode = new Node(val);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  /**
+   * Removes the top item (the head).
+   * - Time: O(1) constant.
+   * - Space: O(1).
+   * @returns {any} The top item of the stack.
+   */
+  pop() {
+    if (this.head === null) {
+      return null;
+    }
+
+    const removed = this.head;
+    this.head = this.head.next;
+
+    return removed.data;
+  }
+
+  /**
+   * Returns the top item of the stack without removing it.
+   * - Time: O(1) constant.
+   * - Space: O(1).
+   * @returns {any} The top item.
+   */
+  peek() {
+    return this.head ? this.head.data : null;
+  }
+
+  /**
+   * Determines if the stack is empty.
+   * - Time: O(1) constant.
+   * - Space: O(1).
+   * @returns {boolean}
+   */
+  isEmpty() {
+    return this.head === null;
+  }
+
+  /**
+   * Gets the count of items in the stack.
+   * - Time: O(n) linear, n = list length.
+   * - Space: O(1).
+   * @returns {number} The total number of items.
+   */
+  size() {
+    let len = 0;
+    let runner = this.head;
+
+    while (runner) {
+      len += 1;
+      runner = runner.next;
+    }
+    return len;
   }
 }
