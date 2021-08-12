7f393441de366ac01b90a9a53ae513ac0debf5fb
Neil M <neilm813@gmail.com>
2021-08-12 11:39:22 -0700

    login reg

--- ForumDemo/Controllers/HomeController.cs
+++ ForumDemo/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ForumDemo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace ForumDemo.Controllers
 {
@@ -19,10 +21,105 @@ namespace ForumDemo.Controllers
             db = context;
         }
 
+        private int? uid
+        {
+            get
+            {
+                return HttpContext.Session.GetInt32("UserId");
+            }
+        }
+
+        private bool isLoggedIn
+        {
+            get
+            {
+                return uid != null;
+            }
+        }
+
         [HttpGet("")]
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                // If email already exists.
+                if (db.Users.Any(u => u.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "is taken.");
+                }
+                /* We could add more custom errors */
+                // if (newUser.FirstName == "Lawrence")
+                // {
+                //     ModelState.AddModelError("FirstName", "Sorry, no Lawrences allowed.");
+                // }
+            }
+
+            // If any above custom errors were added, ModelState would now be invalid.
+            if (ModelState.IsValid == false)
+            {
+                // So error messages are displayed.
+                return View("Index");
+            }
+
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+            newUser.Password = hasher.HashPassword(newUser, newUser.Password);
+
+            db.Users.Add(newUser);
+            db.SaveChanges();
+
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            HttpContext.Session.SetString("FirstName", newUser.FirstName);
+            return RedirectToAction("All", "Posts");
+        }
+
+        [HttpPost("/login")]
+        public IActionResult Login(LoginUser loginUser)
+        {
+            if (ModelState.IsValid == false)
+            {
+                // Display error messages.
+                return View("Index");
+            }
+
+            User dbUser = db.Users.FirstOrDefault(user => user.Email == loginUser.LoginEmail);
+
+            if (dbUser == null)
+            {
+                /*
+                Usually you don't want to reveal specifics, like was the email
+                or password invalid because of phishing. But for testing it's
+                easier to see what actually went wrong with specific errors.
+                */
+                ModelState.AddModelError("LoginEmail", "Email not found.");
+                return View("Index");
+            }
+
+            PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
+            PasswordVerificationResult pwCompareResult = hasher.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+
+            if (pwCompareResult == 0)
+            {
+                // Don't reveal this info, this is just for testing.
+                ModelState.AddModelError("LoginPassword", "Invalid Password");
+                return View("Index");
+            }
+
+            HttpContext.Session.SetInt32("UserId", dbUser.UserId);
+            HttpContext.Session.SetString("FirstName", dbUser.FirstName);
+            return RedirectToAction("All", "Posts");
+        }
+
+        [HttpPost("/logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
